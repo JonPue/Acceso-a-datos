4237a7ba2148ba0541c28fa77b15320967f7542e
JonPue <55479141+JonPue@users.noreply.github.com>
2019-09-26 13:04:02 +0200

    Add files via upload

---
+++ ActividadArchivosDeTexto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace ActividadFicheros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console hasiera!");
+            Console.Write(File.Exists("fichero1.txt"));
+
+            
+            using (StreamWriter outputFile = new StreamWriter("file.txt", true))
+            {
+                outputFile.WriteLine("Linea2");
+            }
+
+            String linea;
+            using (StreamReader sr = new StreamReader("fichero1.txt")){
+                  linea = sr.ReadLine();
+                while((linea = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                }
+            }
+
+            string[] lineass = File.ReadAllLines("demo.csv");
+
+            foreach(var lineas in lineass){
+                var valores = lineas.Split(',');
+                Console.WriteLine("El nombre es: " + valores[0] + "  Nombre usuario: " + valores[1] + "  Direccion de correo: " + valores[2]);
+            }
+
+            
+
+
+            Console.WriteLine("Fin de aplicacion!");
+        }
+    }
+}
