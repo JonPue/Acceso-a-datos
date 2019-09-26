7a1035b065bf429f92377c262bdef9284307a323
JonPue <55479141+JonPue@users.noreply.github.com>
2019-09-26 13:05:38 +0200

    Add files via upload

---
+++ ActividadFicheros/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace ActividadFicheros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console hasiera!");
+
+            String linea;
+            using (StreamReader sr = new StreamReader("fichero1.txt"))
+            {
+                //linea = sr.ReadLine();
+                while((linea = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(linea);
+                }
+            }
+
+
+            Console.WriteLine("Fin de aplicacion!");
+        }
+    }
+}
