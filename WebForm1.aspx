9d2da1b9f42223cc878d2204f9bd5f2931c508af
JonPue <55479141+JonPue@users.noreply.github.com>
2019-09-26 13:09:48 +0200

    Add files via upload

---
+++ WebForm1.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="DropDownListt.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+        </div>
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem Value="1">Manazanas</asp:ListItem>
+            <asp:ListItem Value="2">Platanos</asp:ListItem>
+            <asp:ListItem Value="3">Kiwi</asp:ListItem>
+            <asp:ListItem Value="4">Mango</asp:ListItem>
+        </asp:DropDownList>
+
+        <asp:Button ID="btnSubmit" runat="server" Text="submit" />
+
+
+        <asp:DropDownList ID="DropDownList2" runat="server">
+
+        </asp:DropDownList>
+
+
+        <asp:DropDownList ID="DropDownList3" runat="server">
+        </asp:DropDownList>
+
+
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropDownListt
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string[] frutas = { "Manzanas", "Platanos", "Kiwis" };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DropDownList2.DataSource = frutas;
+            DropDownList2.DataBind();
+
+            //Dictionary
+            Dictionary<int, string> dictFrutas = new Dictionary<int, string>();
+
+            dictFrutas.Add(1, "Manzana");
+            dictFrutas.Add(2, "Platanos");
+            dictFrutas.Add(3, "Kiwis");
+
+            DropDownList3.DataValueField = "Key";
+            DropDownList3.DataTextField = "Value";
+
+            DropDownList3.DataSource = dictFrutas;
+            DropDownList3.DataBind();
+
+        }
+
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            Response.Write(DropDownList1.SelectedValue.ToString());
+        }
+    }
+}
